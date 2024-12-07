09d1744b6f7e6c8ad1ca6b76f7eff08a85398aa3
Dmitriy <dimak712@gmail.com>
2024-12-07 12:41:07 +0500

    add _update_screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,15 +17,17 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+            self._update_screen()    
 
     def _check_events(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-    
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()    
 
 if __name__ == '__main__':
     ai = AlienInvasion()
